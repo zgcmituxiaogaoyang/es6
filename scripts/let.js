246d45b2c1c773fbe58fd4a2a4d40d8e4d7c1753
zgcmituxiaogaoyang <13540641425@163.com>
2017-09-03 20:32:39 +0800

    how to use let command

--- scripts/let.js
+++ scripts/let.js
@@ -54,9 +54,91 @@ let x;
 */
 //TDZ
 /*
+
 var tmp=123;
 if(true){
+	//TDZ开始
 	tmp='123';//ReferenceError: tmp is not defined
-	let tmp;
+	let tmp;//TDZ结束
 }
 */
+//不允许重复声明
+/*
+//报错
+function(){
+	let a=10;
+	var a=1;
+}
+//报错
+function(){
+	let a=10;
+	let a=1;
+}
+//报错
+function(arg){
+	let arg;
+}
+*/
+//不合理场景一:内层变量可能会覆盖外层变量
+/*
+var tmp=new Date();
+function f(){
+	console.log(tmp);
+	if(false){
+		var tmp='hello world!';
+	}
+}
+f();//undefined
+*/
+//不合理场景二:从来计数的循环变量泄露为全局变量
+/*
+var s='hello';
+for(var i=0;i<s.length;i++){
+	console.log(s[i]);
+}
+console.log(i);//5 泄露为全局变量
+*/
+//块级作用域
+/*
+function f(){console.log("I am outside");}
+(function(){
+	if(false){
+		function f(){console.log("I am inside");}
+	}	
+})();
+f();
+*/
+//const用法基本与let相同
+/*
+const foo={};
+foo.pro=123;
+console.log(foo.pro);
+foo={};
+*/
+/*
+const foo=Object.freeze({});//冻结对象
+foo.pro=123;//不起作用
+console.log(foo.pro);//undefined
+*/
+//彻底冻结对象的函数
+/*
+var constantize=(obj)=>{
+	Object.freeze(obj);
+	Object.keys(obj).forEach((key,value)=>{
+		if(typeof obj[key]==='object'){
+			constantize(obj[key]);
+		}
+	});
+};
+const foo={
+	s:123,
+	method:function(){
+		console.log(this.s);
+	}
+};
+constantize(foo);
+foo.key="value";//不起作用
+console.log(foo.key);
+foo.method();
+*/
+
